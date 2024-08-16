faaca760e89cfc4f1faa025b85360ba9a3360e3b
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-08-16 20:53:17 +0200

    finish basic parser layout

--- parser/parser.js
+++ parser/parser.js
@@ -68,11 +68,89 @@ class Parser {
         }
     }
 
-    Expr2R() { }
+    Expr2R() {
+        switch (this.lexer.peek()) {
+            case TOK_PLUS:
+                /* rule 5: Expr2R -> + Expr1 Expr2R */
+                this.lexer(TOK_PLUS);
+                this.Expr1();
+                this.Expr2R();
+                break;
+            case TOK_MINUS:
+                /* rule 6: Expr2R -> - Expr1 Expr2R */
+                this.lexer(TOK_MINUS);
+                this.Expr1();
+                this.Expr2R();
+                break;
+            case TOK_NUMBER:
+            case TOK_LPAR:
+            case TOK_DEGREE:
+            case TOK_FLOATING:
+            case TOK_MINUTE:
+            case TOK_RPAR:
+                /* rule 7: Expr2R ->  */
+                break;
+            default:
+                throw new Error("Parsing error");
+        }
+    }
 
-    Expr1() { }
+    Expr1() {
+        switch (this.lexer.peek()) {
+            case TOK_NUMBER:
+            case TOK_LPAR:
+                /* rule 8: Expr1 -> Expr Expr1R */
+                this.Expr();
+                this.Expr1R();
+                break;
+            default:
+                throw new Error("Parsing error");
+        }
+    }
 
-    Expr1R() { }
+    Expr1R() {
+        switch (this.lexer.peek()) {
+            case TOK_MULTIPLICATION:
+                /* rule 9: Expr1R -> * Expr Expr1R */
+                this.lexer(TOK_MULTIPLICATION);
+                this.Expr1();
+                this.Expr2R();
+                break;
+            case TOK_DIVISION:
+                /* rule 10: Expr1R -> / Expr Expr1R */
+                this.lexer(TOK_DIVISION);
+                this.Expr1();
+                this.Expr2R();
+                break;
+            case TOK_PLUS:
+            case TOK_MINUS:
+            case TOK_NUMBER:
+            case TOK_LPAR:
+            case TOK_DEGREE:
+            case TOK_FLOATING:
+            case TOK_MINUTE:
+            case TOK_RPAR:
+                /* rule 11: Expr1R ->  */
+                break;
+            default:
+                throw new Error("Parsing error");
+        }
+    }
 
-    Expr() { }
+    Expr() {
+        switch (this.lexer.peek()) {
+            case TOK_NUMBER:
+                /* rule 12: Expr -> number */
+                this.lexer.match(TOK_NUMBER);
+                break;
+            case TOK_LPAR:
+                /* rule 13: Expr -> ( Expr2 ) */
+                this.lexer(TOK_LPAR);
+                this.Expr2();
+                this.lexer(TOK_RPAR);
+                break;
+            default:
+                throw new Error("Parsing error");
+        }
+    }
 }
