90f83ab5102d20489a5a70d49eba6f679a6574bb
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-08-16 18:37:00 +0200

    add basic lexing

--- lexer/lexer.js
+++ lexer/lexer.js
@@ -1,3 +1,19 @@
+const TOK_NORTH = -1;
+const TOK_EAST = -2;
+const TOK_DEGREE = -3;
+const TOK_NUMBER = -4;
+const TOK_PLUS = -5;
+const TOK_MINUS = -6;
+const TOK_MULTIPLICATION = -7;
+const TOK_DIVISION = -8;
+const TOK_LPAR = -9;
+const TOK_RPAR = -10;
+const TOK_MINUTE = -11;
+const TOK_EOF = -12;
+const TOK_UNDEFINED = - 13;
+
+//TODO: hashmap for symbol tokens
+
 class Lexer {
     constructor(str) {
         this.str = str;
@@ -9,11 +25,64 @@ class Lexer {
     }
 
     get() {
-        
+        if (this.str[this.idx] == 'N' && this.gcs) {
+            this.gcs = false;
+            this.tok = TOK_NORTH;
+        }
+
+        else if (this.str[this.idx] == 'E' && this.gcs) {
+            this.gcs = false;
+            this.tok = TOK_EAST;
+        }
+
+        else if (this.str[this.idx] == '°') {
+            this.tok = TOK_DEGREE;
+        }
+
+        else if (this.str[this.idx] == '\'') {
+            this.gcs = true;
+            this.tok = TOK_MINUTE;
+        }
+
+        else if (this.str[this.idx] == '+') {
+            this.tok = TOK_PLUS;
+        }
+
+        else if (this.str[this.idx] == '-') {
+            this.tok = TOK_MINUS;
+        }
+
+        else if (this.str[this.idx] == '*') {
+            this.tok = TOK_MULTIPLICATION;
+        }
+
+        else if (this.str[this.idx] == '/') {
+            this.tok = TOK_DIVISION;
+        }
+
+        else if (this.str[this.idx] == '(') {
+            this.tok = TOK_LPAR;
+        }
+
+        else if (this.str[this.idx] == ')') {
+            this.tok = TOK_RPAR;
+        }
+
+        // Whole input has been lexed
+        else if (this.idx == this.str.length) {
+            this.tok = TOK_EOF;
+        }
+
+        else {
+            this.tok = TOK_UNDEFINED;
+        }
+
+        this.idx++;
+        return this.tok;
     }
 
     peek() {
-
+        return this.tok;
     }
 }
 
