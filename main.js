1a2d99167a4ab198c88d2dd3309e021d4d89dd5b
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-08-24 22:49:52 +0200

    update the google map params calculation

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function calculate() {
     let parser = new Parser(coordsString);
     parser.Start();
 
-    window.prompt(parser.coords, `https://www.google.com/maps?q=${parser.latitude + parser.latitude_decimal / 60},${parser.longtitude + parser.longtitude_decimal / 60}`);
+    window.prompt(parser.coords, `https://www.google.com/maps?q=${parseInt(parser.latitude) + parseFloat(parser.latitude_decimal / 60)},${parseInt(parser.longtitude) + parseFloat(parser.longtitude_decimal / 60)}`);
 }
 
 function remove() {
